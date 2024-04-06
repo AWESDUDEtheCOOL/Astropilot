3efd06ad126ffb6a9f178db8eee02c98a87d7b65
kleitherer <kaitlynleitherer@gmail.com>
2024-04-06 16:44:04 -0700

    made the UI a little more cutie

--- landing.py
+++ landing.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')  # Assuming you have an 'index.html' template in the 'templates' directory
 
+
 if __name__ == '__main__':
     app.run(debug=True)
