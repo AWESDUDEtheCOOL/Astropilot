928afb18ecc8b06cace84f86ba997ac16e4e9cc3
kleitherer <kaitlynleitherer@gmail.com>
2024-04-06 17:21:21 -0700

    implemented voice alert

--- landing.py
+++ landing.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from alert import *
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def origin():
     return render_template('index.html')  # Assuming you have an 'index.html' template in the 'templates' directory
 
+@app.route('/alert', methods=['POST'])
+def run_script():
+    # Get text input from the form
+    call_title = 'alert'
+    call_body = request.form['call_body']
+    call_num = request.form['call_num']
+
+    # Call the alert function
+    alert(call_title, call_body, f'+1{call_num}')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
